bf90105c9a398d1bfe9a52af72b27b4be69399c4
renec <renec@ReneKounkou>
2022-04-02 16:55:40 -0400

    Adds new  features for the project

---
+++ weekonetraining/src/main/java/weekonetraining/MyWeekoneTraining.java
@@ -0,0 +1,16 @@
+package weekonetraining;
+
+public class MyWeekoneTraining {
+	
+	public String color;
+	protected int numbofstudents;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ weekonetraining/src/main/java/weekonetraining/MyWeekoneTrainingBase.java
@@ -0,0 +1,16 @@
+package weekonetraining;
+
+public class MyWeekoneTrainingBase extends MyWeekoneTraining {
+	
+	public MyWeekoneTrainingBase() {
+		
+		MyWeekoneTraining newObject = new MyWeekoneTraining();
+		
+		newObject.color = "white";
+		
+		newObject.numbofstudents = 12;
+		
+		newObject.setName("Rene");
+	}
+
+}
